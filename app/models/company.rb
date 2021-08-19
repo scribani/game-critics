fc279144a4a120b5b2a5cef5e26d8a6a20f15861
Scribani <scribani.andres@gmail.com>
2021-08-19 22:43:09 +0000

    Create Company resource

---
+++ app/models/company.rb
@@ -0,0 +1,9 @@
+class Company < ApplicationRecord
+  # Associations
+  has_many :involved_companies, dependent: :destroy
+  has_many :games, through: :involved_companies
+  has_many :critics, as: :criticable, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true, uniqueness: true
+end
