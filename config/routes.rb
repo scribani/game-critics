f5fd45265b67ce72672a4fd5674719cfcfb633a3
ever864 <ever864@mail.com>
2021-08-20 05:35:27 +0000

    Add files devise to config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :critics
+  root to: "home#index"
+
   resources :involved_companies
   resources :genres
   resources :platforms
-  resources :games
+
+  resources :games do
+    resources :critics
+  end
   resources :companies
   resources :users
 end
