d2d1aaeccd90cb2468bf3c1adf54aac82d6d99d9
mindy <minmaglh@gmail.com>
2021-08-20 05:30:59 +0000

    Create controlle-game - action-edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,4 +10,43 @@ class GamesController < ApplicationController
     @main_games = Game.main_game
   end
 
+  # POST /games
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      render "new"
+    end
+  end
+
+  # GET /games/:id/edit
+  def edit
+    @game = Game.find(params[:id])
+  end
+  
+  # PATCH/PUT /games/:id
+  def update
+    @game = Game.find(params[:id])
+
+    if @game.update(game_params)
+      redirect_to @game
+    else
+      render :edit
+    end
+  end
+
+
+  # DELETE /games/:id
+  def destroy
+    game = Game.find(params[:id])
+    game.destroy
+    redirect_to games_path
+  end
+
+  def game_params
+    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id)
+  end
+
 end
