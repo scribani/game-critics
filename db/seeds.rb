c6197abb822349d31ac8e9ca61aeefe9402b28bd
Scribani <scribani.andres@gmail.com>
2021-08-20 07:55:32 +0000

    Add users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'json'
+
+puts "Start seeding"
+# Create random users using Faker
+puts "Seeding users..."
+# Create 1 admin
+User.create(username: "admin", email: "admin@mail.com", role: "admin")
+
+# Create 10 contributors
+10.times do
+  contributor_data = {
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.safe_email,
+  }
+  User.create(contributor_data)
+end
+puts "Finish seeding"
