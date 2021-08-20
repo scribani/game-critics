8ede57dd1b42f2085e8b761ae40049cc39e54684
Scribani <scribani.andres@gmail.com>
2021-08-20 19:46:01 +0000

    Add ActiveStorage to project

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,7 @@ class Company < ApplicationRecord
   has_many :involved_companies, dependent: :destroy
   has_many :games, through: :involved_companies
   has_many :critics, as: :criticable, dependent: :destroy
+  has_one_attached :cover
 
   # Validations
   validates :name, presence: true, uniqueness: true
--- app/models/game.rb
+++ app/models/game.rb
@@ -13,13 +13,15 @@ class Game < ApplicationRecord
                         foreign_key: "parent_id",
                         inverse_of: "parent",
                         dependent: :nullify
+  has_one_attached :cover
 
   # Validations
   validates :name, uniqueness: true, presence: true
   validates :category, presence: true
   validates :rating, numericality: {
     greater_than_or_equal_to: 0,
-    less_than_or_equal_to: 100
+    less_than_or_equal_to: 100,
+    allow_nil: true
   }
   validate :validate_parent
 
