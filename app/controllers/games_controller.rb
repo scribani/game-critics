463ea0bc35a4c46f9706f2331fab7659f63039bb
mindy <minmaglh@gmail.com>
2021-08-21 22:47:11 +0000

    Add controller show - and collection select publisher developer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  include GamesConcern
   # get /games
   def index
     @games = Game.all
@@ -9,9 +10,8 @@ class GamesController < ApplicationController
   def new
     @game = Game.new
     authorize @game
-    
-    @main_games = Game.main_game
 
+    @main_games = Game.main_game
   end
 
   # GET /games/:id
@@ -19,13 +19,13 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     authorize @game
 
-    @genre = Genre.new
-    @platform = Platform.new
+    @genres = @game.genres
+    @platforms = @game.platforms
 
-    @genres = @game.genres.map(&:name)
-    @platforms = @game.platforms.map(&:name)
+    show_pub_dev
+  end
 
-
+  def show_pub_dev
     @developers_total = InvolvedCompany.where(developer: true).map(&:company).uniq
     @developers = @game.involved_companies.where(developer: true).map(&:company)
     @developers_list = (@developers_total - @developers)
@@ -33,11 +33,6 @@ class GamesController < ApplicationController
     @publishers_total = InvolvedCompany.where(publisher: true).map(&:company).uniq
     @publishers = @game.involved_companies.where(publisher: true).map(&:company)
     @publishers_list = (@publishers_total - @publishers)
-
-
-    # @critics = @game.critics
-
-    # @game = Game.all
   end
 
   # POST /games
@@ -79,71 +74,8 @@ class GamesController < ApplicationController
     redirect_to root_path
   end
 
-  # POST /games/:id/add_genre
-  def add_genre
-    game = Game.find(params[:id])
-    authorize game
-
-    genre = Genre.find(params[:genre][:id])
-    game.genres << genre
-
-    redirect_to game
-  end
-
-  # POST /games/:id/add_genre
-  def add_platform
-    game = Game.find(params[:id])
-    authorize game
-
-    platform = Platform.find(params[:platform][:id])
-    game.platforms << platform
-
-    redirect_to game
-  end
-
-  # POST /games/:id/developers
-  def add_developer
-    @game = Game.find(params[:id])
-    authorize @game
-
-    company = Company.find(params[:develop_id])
-    @game.companies << company
-    comp_dev = @game.involved_companies.where(company_id: company).first
-    comp_dev.developer = true
-    comp_dev.save
-
-    redirect_to @game
-  end
-
-  # POST /games/:id/publishers
-  def add_publisher
-    @game = Game.find(params[:id])
-    authorize @game
-
-    company = Company.find(params[:publisher_id])
-    @game.companies << company
-    comp_pub = @game.involved_companies.where(company_id: company).first
-    comp_pub.publisher = true
-    comp_pub.save
-
-    redirect_to @game
-  end
-
-  # -----------DELETE
-  # DELETE /games/:id/publisher
-  def remove_publisher
-    @game = Game.find(params[:id])
-    authorize @game
-
-    company = Company.find(params[:publisher_id])
-    comp_pub = @game.involved_companies.where(company_id: company).first
-    @game.involved_companies.delete(comp_pub)
-
-    redirect_to @game
-  end
-
   def game_params
-    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id, :cover)
+    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id,
+                                 :cover)
   end
-
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,7 +7,7 @@ class Game < ApplicationRecord
 
   has_many :involved_companies, dependent: :destroy
   has_many :companies, through: :involved_companies
-  
+
   has_many :critics, as: :criticable, dependent: :destroy
   has_and_belongs_to_many :platforms
   has_and_belongs_to_many :genres
